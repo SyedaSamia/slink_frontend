09f3e71d034ee008dc0d86bae7f24c5f76c6521b
syedasamia <samia054@gmail.com>
2021-11-17 16:17:38 +0100

    button color and onClick method added

--- src/components/Input/Button.vue
+++ src/components/Input/Button.vue
@@ -1,17 +1,23 @@
 <template>
-  <button>{{ title }}</button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <script>
 export default {
-    name: 'Button',
-    props: {
-        title: String
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      
+      console.log('click')
     }
+  }
 
 }
 </script>
 
-<style>
-
-</style>
