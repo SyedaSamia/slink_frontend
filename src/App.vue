c3bacebf5fe6a555dd434279d2297933583887e8
syedasamia <samia054@gmail.com>
2021-11-28 12:58:26 +0100

    rearranged components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,24 @@
   <q-layout view="hHh lpR fFf">
     <Header title="SLINK"/>
     <q-page-container>
-      <Main />
+      <div class="q-py-lg">
+        <q-form @submit.prevent="submit(longUrl)">
+          <div class="row justify-center">
+            <div class="col-6">
+              <q-input color="blue" filled type="url" outlined :dense="dense" label="Enter Long Url" v-model="longUrl"/>
+            </div>
+          </div>
+          <div class="q-py-lg">
+            <div class="row justify-center">
+                <q-btn type="submit" color="primary" label="Get SLINK" />
+            </div>
+          </div>
+
+        </q-form>
+
+        <Output :shortUrl="shortUrl"/>
+
+      </div>
 
     </q-page-container>
   </q-layout>
@@ -12,17 +29,61 @@
 <script>
 import { ref } from 'vue'
 import Header from './components/Header.vue'
-import Main from './components/Main.vue'
+import Output from './components/Output.vue'
+import axios from 'axios'
+
 
 
 export default {
   name: 'App',
   components: {
     Header,
-    Main
+     Output,
+
   },
 
+data() {
+        return {
+            output : 'This is your short url....xxxxxxx',
+            longUrl : '',
+            shortUrl : ''
 
+        }
+    },
+     methods: {
+
+       async submit(longUrl) {
+         console.log("XYZ " + longUrl)
+
+        try {
+          const headers = {
+
+     "Access-Control-Allow-Origin": "*",
+     "Access-Control-Allow-Methods" : "POST, GET",
+     "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//     "Access-Control-Request-Method" : "POST",
+  //   "Access-Control-Request-Headers": "Content-Type, Authorization",
+     "Content-Type": "application/json",
+  };
+          const api = "https://slnk-app.herokuapp.com/api/url/shorten/";
+       await axios.post(api, {"longUrl": longUrl}, { headers }).then(
+          response => {
+            this.shortUrl = response.data.shortUrl
+            console.log(this.shortUrl)
+          }
+        )
+        }
+        catch(error ) {
+          console.log("XYZ " + error)
+        }
+
+      }
+
+      // onClick () {
+      //   this.output = 'Changed output after button click'
+      // },
+
+    },
 
   setup () {
     return {
